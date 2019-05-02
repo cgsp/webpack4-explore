930ac40c637f8b31287317203fbdc54a9959512c
guansaipeng <cgsp740178088@163.com>
2019-05-02 23:36:12 +0800

    基础配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,23 @@
  * @Author: John.Guan
  * @Date: 2019-05-02 23:14:34
  * @Last Modified by: John.Guan
- * @Last Modified time: 2019-05-02 23:16:45
+ * @Last Modified time: 2019-05-02 23:34:59
  */
-
-modules.exports = {xii
+// node的内置模块，不需要npm安装
+const path = require('path')
+/** 
+ * commonjs规范
+ * 默认必须是webpack.config.js这个文件
+ * 如果想变更，不用webpack.config.js这个文件名，需要运行npx webpack --config xxx.js
+ */
+module.exports = {
+  entry: './index.js', // 入口
+  output: {
+    // 打包出来的文件叫什么
+    filename: 'bundle.js',
+    // 打包出来的文件，放在什么地方；
+    // path这个模块，有个resolve方法，这个方法，第一个参数__dirname，意思是说，与webpack.config.js所在的目录同级的地方；
+    // path.resolve(__dirname, 'build')意思是说，在webpack.config.js的同级目录创建一个build文件，用来放置打包好的bundle.js
+    path: path.resolve(__dirname, 'build'),
+  }
 }
