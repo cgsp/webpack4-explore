e527f2a433827ce59a7cf6311053505adaa06d97
guansaipeng <cgsp740178088@163.com>
2019-05-02 23:17:13 +0800

    配置文件

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Desc: webpack的配置文件
+ * @Author: John.Guan
+ * @Date: 2019-05-02 23:14:34
+ * @Last Modified by: John.Guan
+ * @Last Modified time: 2019-05-02 23:16:45
+ */
+
+modules.exports = {xii
+}
