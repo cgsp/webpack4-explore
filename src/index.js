3d9ef6290e5d91d387f852674b078e5423360b82
guansaipeng <cgsp740178088@163.com>
2019-05-31 10:44:42 +0800

    测试babel/preset-react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 
-// import '@babel/polyfill'
-
-import header from './header'
-
+import '@babel/polyfill'
 import './index.scss'
 
-header()
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>我是一个react</div>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
+
+
 
