94682d0f0562a37dc6e460aaa9291399c3c3b3bc
guansaipeng <cgsp740178088@163.com>
2019-06-08 22:37:05 +0800

    splitchunksplugin

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@
 // import '@babel/polyfill'
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
+import _ from 'lodash'
 import './index.scss'
 
-const getComponent = () => {
-  // 获取lodash，然后赋值给_这个变量
-  return import('lodash').then(({ default: _ }) => {
-    var ele = document.createElement('div')
-    ele.innerHTML = _.join(['g', 's'], '^^^')
-    return ele
-  })
-}
+// const getComponent = () => {
+//   // 获取lodash，然后赋值给_这个变量
+//   return import(/* webpackChunkName:"lodash"*/'lodash').then(({ default: _ }) => {
+//     var ele = document.createElement('div')
+//     ele.innerHTML = _.join(['g', 's'], '^^^')
+//     return ele
+//   })
+// }
 
-getComponent()
-  .then(ele => {
-    document.body.appendChild(ele)
-  })
+// getComponent()
+//   .then(ele => {
+//     document.body.appendChild(ele)
+// })
 
 export default class App extends Component {
   render() {
