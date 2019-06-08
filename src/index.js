c0d8150a55032c8ad4212a06100294d1f935e214
guansaipeng <cgsp740178088@163.com>
2019-06-08 16:31:08 +0800

    code spliting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 
 // import '@babel/polyfill'
-import './index.scss'
-
 import React, { Component } from 'react'
 import ReactDom from 'react-dom'
-import { add } from './math'
+import './index.scss'
+
+const getComponent = () => {
+  // 获取lodash，然后赋值给_这个变量
+  return import('lodash').then(({ default: _ }) => {
+    var ele = document.createElement('div')
+    ele.innerHTML = _.join(['g', 's'], '^^^')
+    return ele
+  })
+}
+
+getComponent()
+  .then(ele => {
+    document.body.appendChild(ele)
+  })
 
 export default class App extends Component {
   render() {
-    add()
     return (
       <div>我是一个react9992</div>
     )
