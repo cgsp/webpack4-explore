363e741029d64d710298dbee512077ea01ac445a
guansaipeng <cgsp740178088@163.com>
2019-07-17 17:28:12 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     library: 'gsp', // 支持script标签引入的，希望通过gsp这个全局变量去使用这个库的方法
-    libraryTarget: 'umd' // umd的u的意思是unversial，通用的意思，代表支持，esmodule, common.js, 和AMD规范
+    libraryTarget: 'umd', // umd的u的意思是unversial，通用的意思，代表支持，esmodule, common.js, 和AMD规范
+    libraryExport: 'default'
 
     // libraryTarget: 'window' ,
   }
