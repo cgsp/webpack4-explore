0c727711be8ee9b82c0022a451fbae2836b95574
guansaipeng <cgsp740178088@163.com>
2019-07-20 20:05:03 +0800

    恢复

---
+++ build/webpack.prod.js
@@ -0,0 +1,42 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const merge = require('webpack-merge')
+const commonConfig = require('./webpack.common')
+
+const prodConfig = {
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          'sass-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:8].css',
+      chunkFilename: '[name].chunk.[hash:8].css'
+    })
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js'
+  }
+}
+
+modules.exports = merge(commonConfig, prodConfig)
